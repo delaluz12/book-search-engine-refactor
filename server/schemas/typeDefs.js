f68794b679802dc319782970ad0d31183696496b
delaluz12 <75863526+delaluz12@users.noreply.github.com>
2021-11-19 16:28:52 -0600

    added User type to typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,36 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+        savedBooks: [Book]!
+    }
+
+    
+
+    # Set up an Auth type to handle returning data from a profile creating or user login
+    type Auth {
+    token: ID!
+    user: User
+  }
+  type Query {
+      users: [User]!
+      user:(userId: ID!):User
+
+  }
+
+  type Mutation {
+       # Set up mutations to handle creating a profile or logging into a profile and return Auth type
+       addUser(username: String!, email: String!, password: String!): Auth
+       login(username: String!, password: String!):Auth
+       addBook(userId: ID!, book: String!): User
+       removeBook(userId: ID!, bookId: ID!): user
+
+  }
+
+
+
+`;
