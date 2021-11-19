e77a39638db3e8db71ebf2708c0e95471de404ad
delaluz12 <75863526+delaluz12@users.noreply.github.com>
2021-11-19 17:06:49 -0600

    added book type to typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -9,7 +9,14 @@ const typeDefs = gql`
         savedBooks: [Book]!
     }
 
-    
+    type Book {
+        authors: [String]!
+        description: String
+        bookId: String
+        image: String
+        link: String
+        title: String
+    }
 
     # Set up an Auth type to handle returning data from a profile creating or user login
     type Auth {
@@ -18,14 +25,15 @@ const typeDefs = gql`
   }
   type Query {
       users: [User]!
-      user:(userId: ID!):User
+      user:(userId: ID!, username: String): User
+
 
   }
 
   type Mutation {
        # Set up mutations to handle creating a profile or logging into a profile and return Auth type
        addUser(username: String!, email: String!, password: String!): Auth
-       login(username: String!, password: String!):Auth
+       login(username: String!, email: String, password: String!):Auth
        addBook(userId: ID!, book: String!): User
        removeBook(userId: ID!, bookId: ID!): user
 
