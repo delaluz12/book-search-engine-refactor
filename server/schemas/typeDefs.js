f117150a96f2ab32b414c99aa2a9ddfae7971d25
delaluz12 <75863526+delaluz12@users.noreply.github.com>
2021-11-20 09:41:00 -0600

    made changed to meet reqs on guide

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,58 +1,53 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-    type User {
-        _id: ID!
-        username: String
-        email: String!
-        # might not need to include password here..
-        password: String!
-        savedBooks: [Book]!
-    }
+  type User {
+    _id: ID!
+    username: String
+    email: String!
+    # might not need to include password here..
+    # password: String!
+    bookCount: Int
+    savedBooks: [Book]!
+  }
 
-    type Book {
-        _id: ID!
-        authors: [String]!
-        description: String
-        image: String
-        link: String
-        title: String
-    }
-    # want to be able to add book and update book list so we need an input
-    input savedBookInput {
-        authors: [String]
-        description: String!
-        bookId: String!
-        image: String
-        link: String
-        title: String!
-    }
+  type Book {
+    bookId: String!
+    authors: [String]!
+    description: String
+    image: String
+    link: String
+    title: String
+  }
+  # want to be able to add book and update book list so we need an input
+  input savedBookInput {
+    bookId: String!
+    authors: [String]
+    description: String!
+    image: String
+    link: String
+    title: String!
+  }
 
-    # Set up an Auth type to handle returning data from a profile creating or user login
-    type Auth {
+  # Set up an Auth type to handle returning data from a profile creating or user login
+  type Auth {
     token: ID!
     user: User
   }
   type Query {
-      users: [User]!
-      user:(userId: ID!, username: String): User
-      me: User
-
+    #   users: [User]! ==not needed according to project readme
+    #   user:(userId: ID!, username: String): User  ==not needed according to project readme
+    me: User
   }
 
   type Mutation {
-       # Set up mutations to handle creating a profile or logging into a profile and return Auth type
-       addUser(username: String!, email: String!, password: String!): Auth
-       login( email: String!, password: String!):Auth
-       #setup mutations to add book to savedbooks arr and one to remove from arr
-       addBook(input: savedBookInput
-       !): User
-       removeBook(bookId: ID!): User
-
+    # Set up mutations to handle creating a profile or logging into a profile and return Auth type
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    #setup mutations to add book to savedbooks arr and one to remove from arr
+    addBook(input: savedBookInput!): User
+    removeBook(bookId: ID!): User
   }
-
-
-
 `;
 
 module.exports = typeDefs;
