c26f553c830f87d187aa48c7024745c15ec05753
delaluz12 <75863526+delaluz12@users.noreply.github.com>
2021-11-22 21:42:17 -0600

    incl prod build

---
+++ client/build/precache-manifest.ec40d9c2160c31ade675151baaa2fec1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5056a790fa367eadb89f8b647e10605",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77b3f032659ea8e191bd",
+    "url": "/static/css/2.429e79c4.chunk.css"
+  },
+  {
+    "revision": "6c341b1f40f3bdc2cbb6",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "77b3f032659ea8e191bd",
+    "url": "/static/js/2.0db91f0a.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.0db91f0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c341b1f40f3bdc2cbb6",
+    "url": "/static/js/main.9ba6b8e9.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
